a537b575d581939ca4e796bf35ff3c42bfb548ea
Duncan Kariuki <duncankariukimugo@gmail.com>
2025-03-23 07:00:32 +0300

    adds index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const init = () => {
-  
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();})
 }
 
 document.addEventListener('DOMContentLoaded', init);
+const inputForm = document.querySelector("form");
+inputForm.addEventListener("submit", (event) => {});
